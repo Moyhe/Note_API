779b7173e679724d33ec2d064bb8a1a635aeab33
Mohye <dragonsinmeliodas777@gmail.com>
2023-11-23 04:02:17 +0200

    Adding index function for listing notes using resources

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\NoteResource;
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $notes = Note::all();
+
+        if (empty($notes)) {
+            return $this->sendError('No Notes Listed yet');
+        }
+
+        return $this->sendResponse(NoteResource::collection($notes), 'notes was retreived successfully');
+    }
 }
